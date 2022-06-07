41e7b0fd91b22674675cfa82acfd55beb3d161ca
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-06-07 14:57:40 +0200

    Add project files.

---
+++ X_Android/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace X_Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public EditText Edt_Input { get; set; }
+        public Button Btn_Ok { get; set; }
+        public Button Btn_Google { get; set; }
+        public Button Btn_ShowPicture { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Edt_Input = FindViewById<EditText>(Resource.Id.activity_main_Edt_Input);
+            Edt_Input.Click += ShowToast;
+            //Btn_Ok = FindViewById<Button>(2131230778);
+            Btn_Ok = FindViewById<Button>(Resource.Id.activity_main_Btn_Ok);
+
+            Btn_Ok.Click += ShowToast;
+            //Btn_Ok.Click += (sender, e) => Toast.MakeText(this, $"Ihre gewählte Zahl ist {Edt_Input.Text}.", ToastLength.Long).Show();
+
+            Btn_Google = FindViewById<Button>(Resource.Id.activity_main_Btn_Google);
+
+            Intent impliziterIntent = new Intent(Intent.ActionView, Android.Net.Uri.Parse("https://www.google.de"));
+            Btn_Google.Click += (s, e) => StartActivity(impliziterIntent);
+
+            Btn_ShowPicture = FindViewById<Button>(Resource.Id.activity_main_Btn_ShowPicture);
+            Intent expliziterIntet = new Intent(this, typeof(ShowPictureActivity));
+            Btn_ShowPicture.Click += (s,e) => StartActivity(expliziterIntet);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void ShowToast(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, $"Ihre gewählte Zahl ist {Edt_Input.Text}.", ToastLength.Long).Show();
+        }
+    }
+}
---
+++ X_Android/ShowPictureActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X_Android
+{
+    [Activity(Label = "ShowPictureActivity", Theme = "@style/AppTheme")]
+    public class ShowPictureActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.activity_showpicture);
+        }
+    }
+}
